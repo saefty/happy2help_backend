71c6a9a9383ee1f7f320035b8eaeb1b0ad8f0aee
U-LAPTOP-FI28QVC3\schac <s67491@beuth-hochschule.de>
2018-10-27 11:27:29 +0200

    added states for participation

--- H2H/Happy2Help/models.py
+++ H2H/Happy2Help/models.py
@@ -38,11 +38,11 @@ class Job(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    openpositions = models.IntegerField()
+    openpositions = models.IntegerField(default=999)
 
 
     def __str__(self):
-        return self.name
+        return str(self.name) + " at the event " + str(self.event)
 
 
 class Rating(models.Model):
@@ -60,12 +60,23 @@ class Rating(models.Model):
 
 class Participation(models.Model):
     #event = models.ForeignKey(Event, on_delete=models.SET_NULL, blank=False, null=True) # partip nicht löschen wenn event gelöscht wird. etwas besseres als NULL wär gut...
+    PARTICIPATION_STATES = (
+        ('PA', 'Participated'),
+        ('AP', 'Applied'),
+        ('DE', 'Declined'),
+        ('AC', 'Accepted'),
+    )
+
     job = models.ForeignKey(Job, on_delete=models.SET_NULL, blank=False, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE) # wenn user gelöscht, dann ist particip auch weg
     rating = models.ForeignKey(Rating, on_delete=models.SET_NULL, blank=True, null=True)
+    state = models.CharField(max_length=2, choices=PARTICIPATION_STATES, default='AP')
+
+
+    
 
     def __str__(self):
-        return str(self.user) + ' attends ' + str(self.event)
+        return str(self.user) + ' attends ' + str(self.job)
 
 
 class Favourite(models.Model):
