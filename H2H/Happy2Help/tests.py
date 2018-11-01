8b6d5d718819b258f0b06df1428261ea772b9137
Nicolai Brandt <brandt.nicolai@gmail.com>
2018-11-01 17:14:58 +0100

    add tests

--- H2H/Happy2Help/tests.py
+++ H2H/Happy2Help/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from graphene.test import Client
+from H2H.H2H.schema import schema
+
 
 # Create your tests here.
+class UserModelTests(TestCase):
+    client = Client(schema)
+
+    def test_user_creation(self):
+        result = self.client.execute("""mutation {createUser(username:"tOor" email:"toor@gmail.com" password:"testtest") {user {username}}}""")
