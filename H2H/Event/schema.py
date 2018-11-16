94165181f7f188f10a1241571ce84fdf99afa2fc
rosenthaleric <s69478@beuth-hochschule.de>
2018-11-16 11:53:57 +0100

    added user password resrtictor and missing participation mutations

--- H2H/Event/schema.py
+++ H2H/Event/schema.py
@@ -280,6 +280,9 @@ class Query(graphene.ObjectType):
 
 # Mutations
 class Mutation(graphene.AbstractType):
+    create_participation = CreateParticipation.Field()
+    update_participation = UpdateParticipation.Field()
+
     create_job = CreateJob.Field()
     update_job = UpdateJob.Field()
     delete_job = DeleteJob.Field()
