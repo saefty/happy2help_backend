fa15467a622e8153eaf4d4113d038003cca65163
Nicolai Brandt <brandt.nicolai@gmail.com>
2018-11-21 09:16:54 +0100

    add create event test

--- H2H/Event/tests.py
+++ H2H/Event/tests.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 
 from graphql_jwt.testcases import JSONWebTokenTestCase
 
+from Event.models import Event
 from Location.models import Location
 
 
@@ -9,7 +10,6 @@ class TestEvent(JSONWebTokenTestCase):
     @classmethod
     def setUpTestData(cls):
         cls.user = get_user_model().objects.create(username='tOor', password='testtest')
-        cls.location = Location.objects.create(latitude=52.564668, longitude=13.360665, name="Schäfersee")
 
     def setUp(self):
         self.client.authenticate(self.user)
@@ -17,7 +17,7 @@ class TestEvent(JSONWebTokenTestCase):
     def test_create_event(self):
         # create location first
         # TODO: maybe create an empty location at event creation
-
+        location = Location.objects.create(latitude=52.564668, longitude=13.360665, name="Schäfersee")
         resp = self.client.execute(
             """
             mutation {
@@ -37,3 +37,24 @@ class TestEvent(JSONWebTokenTestCase):
 
     def test_delete_event_with_participations(self):
         pass
+
+    def test_create_job(self):
+        location = Location.objects.create(latitude=52.544937, longitude=13.351855, name="Beuth")
+        event = Event.objects.create(
+            name="Mega Event",
+            description="Da muss man hin!",
+            creator=self.user,
+            location=location
+        )
+        resp = self.client.execute(
+            """
+            mutation {
+                createJob(eventId:1 name:"ein job" description:"toller job" totalPositions:2) {
+                job {
+                  id
+                }
+              }
+            }
+            """
+        )
+        self.assertTrue(resp.data["createJob"]["job"]["id"])
