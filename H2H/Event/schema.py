231aca5624abe1cbaf2781e3e533bb70f1ea7b53
Nicolai Brandt <brandt.nicolai@gmail.com>
2019-01-24 01:37:23 +0100

    fixed updateParticipation to allow members of organisation to accept applicants

--- H2H/Event/schema.py
+++ H2H/Event/schema.py
@@ -132,8 +132,12 @@ class UpdateParticipation(graphene.Mutation):
             )
 
         if state in (3, 4, 1):  # 3 = declined, 4 = accepted, 1 = participated
-            if user != event_creator and user not in job.event.organisation.members.all():
-                raise Exception("You need to be the event creator or in its organisations members")
+            print("A")
+            if user != event_creator:
+                print("B")
+                if user not in job.event.organisation.members.all():
+                    print("C")
+                    raise Exception("You need to be the event creator or in its organisations members")
             if state == 4 and job.deleted_at:
                 raise Exception("You cannot accept a user for a removed job")
             if state == 1:
